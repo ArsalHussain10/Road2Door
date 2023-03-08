d38575ef985e5842a14e330f4dacd39653ca488f
syedaLaiba <laiba033545@gmail.com>
2023-03-08 17:11:13 +0500

    added file uploder code with ajax

--- Road2Door/Controllers/RiderController.cs
+++ Road2Door/Controllers/RiderController.cs
@@ -1,11 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Road2Door.Models;
 using Road2Door.Models.Repository;
+using System.IO;
 
 namespace Road2Door.Controllers
 {
     public class RiderController : Controller
     {
+        private readonly IWebHostEnvironment Environment;
+        public RiderController(IWebHostEnvironment
+           environment)
+        {
+
+            Environment = environment;
+        }
         [HttpGet]
         public IActionResult SignUp()
         {
@@ -20,23 +28,80 @@ namespace Road2Door.Controllers
             string password = form["password"];
             string cnic = form["cnic"];
             string phone = form["phoneNumber"];
-            IFormFile license = form.Files.FirstOrDefault(x => x.Name == "License");
-            IFormFile policeRecord = form.Files.FirstOrDefault(x => x.Name == "CriminalRecord");
+            List<IFormFile> files = new List<IFormFile>();
+            files = Request.Form.Files.ToList();
+            //IFormFile license = form.Files.FirstOrDefault(x => x.Name == "License");
+            //IFormFile policeRecord = form.Files.FirstOrDefault(x => x.Name == "CriminalRecord");
 
-            string licenseName = license.FileName;
-            string policeRecordName = policeRecord.FileName;
+            string wwwPath = this.Environment.WebRootPath;
+            string subPath1 = "uploads\\Licnese\\";
+            string subPath2 = "uploads\\CriminalRecord\\";
+            string path1 = Path.Combine(wwwPath, subPath1);
+            string path2 = Path.Combine(wwwPath, subPath2);
 
-            Rider rider = new Rider {
+            
+            if (!Directory.Exists(path1))
+            {
+                Directory.CreateDirectory(path1);
+            }
+            if (!Directory.Exists(path2))
+            {
+                Directory.CreateDirectory(path2);
+            }
+            int i = 0;
+            var license = "";
+            var policeRecord = "";
+            foreach (var file in files)
+            {
+                if (i == 0)
+                {
+                     license = Path.GetFileName(file.FileName);
+
+                    var pathWithFileName = Path.Combine(path1, license);
+                    i++;
+                    using (FileStream stream = new
+                        FileStream(pathWithFileName,
+                        FileMode.Create))
+                    {
+                        file.CopyTo(stream);
+                    }
+                }
+                else
+                {
+                    policeRecord = Path.GetFileName(file.FileName);
+
+                    var pathWithFileName = Path.Combine(path2, policeRecord);
+                    using (FileStream stream = new
+                        FileStream(pathWithFileName,
+                        FileMode.Create))
+                    {
+                        file.CopyTo(stream);
+                    }
+                }
+            }
+            Rider rider = new Rider();
+            rider.Name = name;
+            rider.Email = email;
+            rider.Password = password;
+            rider.Cnic = cnic;
+            rider.Phone = phone;
+            rider.License = license;
+            rider.PoliceRecord = policeRecord;
+            rider.Status = 0;
+
+         /*   Rider rider1 = new Rider {
                 Name = name,
                 Email = email,
                 Password = password,
                 Cnic = cnic,
                 Phone = phone,
-                License = licenseName,
-                PoliceRecord = policeRecordName,
+                License = license,
+                PoliceRecord = policeRecord,
                 Status = 0
                 
             };
+
+         */  
             RiderRepository riderRepository= new RiderRepository();
             riderRepository.SignUp(rider);
             
