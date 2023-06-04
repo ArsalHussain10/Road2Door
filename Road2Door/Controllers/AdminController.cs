75fa57e157d83c7f246ebd81681976441c3e8401
ArsalHussain10 <muhammad.arsal236@gmail.com>
2023-06-05 03:36:54 +0500

    started admin panel

--- Road2Door/Controllers/AdminController.cs
+++ Road2Door/Controllers/AdminController.cs
@@ -1,8 +1,32 @@
-﻿namespace Road2Door.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using Road2Door.Models;
+using Road2Door.Models.Repository;
+
+namespace Road2Door.Controllers
 {
-    public class Admin
+    public class AdminController : Controller
     {
-        //testing
-        //hello
+        [HttpGet]
+        public IActionResult AdminHome()
+        {
+            return View();
+        }
+
+        public IActionResult ShowRiders()
+        {
+            AdminRepository adminRepository = new AdminRepository();
+            List<Rider> riders=adminRepository.GetRiders();
+
+            
+            return View(riders);
+        }
+        public IActionResult ShowConsumers()
+        {
+            AdminRepository adminRepository = new AdminRepository();
+            List<Consumer> consumers=adminRepository.GetConsumers();
+
+            
+            return View(consumers);
+        }
     }
 }
---
+++ Road2Door/Models/Repository/AdminRepository.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Road2Door.Models.Repository
+{
+    public class AdminRepository
+    {
+        public AdminRepository() { }
+
+        public List<Rider> GetRiders()
+        {
+            Road2DoorContext road2DoorContext= new Road2DoorContext();
+            return road2DoorContext.Riders.ToList();
+
+        }
+        [HttpGet]
+        public List<Consumer> GetConsumers()
+        {
+            Road2DoorContext road2DoorContext= new Road2DoorContext();
+            return road2DoorContext.Consumers.ToList();
+
+        }
+    }
+}
